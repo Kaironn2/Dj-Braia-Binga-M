fd132cdbe6730a84eb0861f9f31b525572712195
Jonathas <jonathas_oliveira17@hotmail.com>
2023-08-16 01:32:03 -0300

    music check added
    check music play added
    toggle play/pause fixed

--- mp3_player.py
+++ mp3_player.py
@@ -6,38 +6,37 @@ import os
 class DJBraiaBingaM:
     def __init__(self, root):
         self.root = root
-        self.root.title("DJ Braia Binga M")         # window title
+        self.root.title("DJ Braia Binga M")
 
         self.playlist = []
         self.current_index = 0
         self.playing = False
 
-        self.load_button = tk.Button(root, text="Load MP3", command=self.load_mp3)              # load mp3 button
+        self.load_button = tk.Button(root, text="Load MP3", command=self.load_mp3)
         self.load_button.pack(pady=10)
 
-        self.playlist_display = tk.Listbox(root, selectmode=tk.SINGLE, height=10, width=30)     # playlist
+        self.playlist_display = tk.Listbox(root, selectmode=tk.SINGLE, height=10, width=30)
         self.playlist_display.pack()
         self.playlist_display.bind("<<ListboxSelect>>", self.update_current_index)
-        
-        self.current_song_label = tk.Label(root, text="Current Song:")                          # current song text
+
+        self.current_song_label = tk.Label(root, text="Current Song:")
         self.current_song_label.pack()
 
-        self.current_song_display = tk.Label(root, text="")                                     # current song
+        self.current_song_display = tk.Label(root, text="")
         self.current_song_display.pack()
-        
-        self.volume_scale = tk.Scale(root, from_=0, to=1, resolution=0.1, orient=tk.HORIZONTAL, label="Volume", command=self.set_volume)    # volume
+
+        self.volume_scale = tk.Scale(root, from_=0, to=1, resolution=0.1, orient=tk.HORIZONTAL, label="Volume", command=self.set_volume)
         self.volume_scale.set(0.5)
         self.volume_scale.pack()
-        
+
         self.play_stop_frame = tk.Frame(root)
-        self.prev_button = tk.Button(self.play_stop_frame, text="Previous", command=self.play_previous)     # previous button
+        self.prev_button = tk.Button(self.play_stop_frame, text="Previous", command=self.play_previous)
         self.prev_button.pack(side=tk.LEFT, padx=5)
-        self.play_button = tk.Button(self.play_stop_frame, text="Play", command=self.toggle_play_pause)     # play/pause button    
+        self.play_button = tk.Button(self.play_stop_frame, text="Play", command=self.toggle_play_pause)
         self.play_button.pack(side=tk.LEFT, padx=5)
-        self.next_button = tk.Button(self.play_stop_frame, text="Next", command=self.play_next)             # next button
+        self.next_button = tk.Button(self.play_stop_frame, text="Next", command=self.play_next)
         self.next_button.pack(side=tk.LEFT, padx=5)
         self.play_stop_frame.pack()
-
 
     def load_mp3(self):
         mp3_files = filedialog.askopenfilenames(filetypes=[("MP3 files", "*.mp3")])
@@ -48,19 +47,23 @@ class DJBraiaBingaM:
     def toggle_play_pause(self):
         if self.playlist:
             if not pygame.mixer.get_init():
-                pygame.mixer.init()  # Inicialize o mixer se ainda não estiver inicializado
-            if self.playing:
+                pygame.mixer.init()
+            
+            if pygame.mixer.music.get_busy() and self.playing:
                 pygame.mixer.music.pause()
                 self.play_button.config(text="Play")
-            else:
-                if pygame.mixer.music.get_busy():
-                    pygame.mixer.music.unpause()
-                else:
-                    pygame.mixer.music.load(self.playlist[self.current_index])
-                    pygame.mixer.music.play()
+            elif pygame.mixer.music.get_busy() and not self.playing:
+                pygame.mixer.music.unpause()
                 self.play_button.config(text="Pause")
-            self.playing = not self.playing
-
+            else:
+                self.play_music()
+    
+    def play_music(self):
+        pygame.mixer.music.load(self.playlist[self.current_index])
+        pygame.mixer.music.play()
+        self.play_button.config(text="Pause")
+        self.playing = True
+    
     def play_previous(self):
         if self.current_index > 0:
             self.current_index -= 1
@@ -76,10 +79,36 @@ class DJBraiaBingaM:
 
     def play(self):
         if self.playlist:
-            pygame.mixer.init()
+            if not pygame.mixer.get_init():
+                pygame.mixer.init()
             pygame.mixer.music.load(self.playlist[self.current_index])
             pygame.mixer.music.play()
             self.update_current_song_display()
+
+            pygame.mixer.music.set_endevent(pygame.USEREVENT)
+            self.check_music_end()
+
+    def check_music_playing(self):
+        if pygame.mixer.music.get_busy() and not self.playing:
+            self.play_button.config(text="Pause")
+            self.playing = True
+        elif not pygame.mixer.music.get_busy() and self.playing:
+            self.play_button.config(text="Play")
+            self.playing = False
+        self.root.after(100, self.check_music_playing)
+        
+    def check_music_end(self):
+        for event in pygame.event.get():
+            if event.type == pygame.USEREVENT:
+                self.play_next_auto()
+        self.root.after(100, self.check_music_end)
+
+    def play_next_auto(self):
+        if self.current_index < len(self.playlist) - 1:
+            self.current_index += 1
+            self.play()
+        else:
+            pygame.mixer.music.stop()
 
     def update_playlist_display(self):
         self.playlist_display.delete(0, tk.END)
@@ -98,6 +127,6 @@ class DJBraiaBingaM:
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("400x500")            # window lenght
+    root.geometry("400x500")
     dj_player = DJBraiaBingaM(root)
     root.mainloop()
